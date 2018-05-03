05b8d6a0577220af951dda6048a24a30fd6981eb
Carlos <carlosngsang.cs@gmail.com>
2018-05-03 13:47:59 -0400

    merging front-end

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,18 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/mydb');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log('rendering home');
-  res.render('home', {user:req.user});
+router.get('/', function(req, res) {
+  // console.log('rendering home');
+  res.render('mainproject', {user:req.user});
 });
+
+// router.get('/login',function (req,res){
+//   res.render('login');
+// });
+
+router.get('/video',function (req,res){
+  res.render('video');
+});
+
 
 module.exports = router;
